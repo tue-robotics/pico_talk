d3d714b058f38442dfc01b1ee878a1a958feb1f7
KdVos <50488480+KdVos@users.noreply.github.com>
2022-06-03 17:04:57 +0200

    Maintenance and Pyro Updates

--- scripts/speech_server.py
+++ scripts/speech_server.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 
 import rospy
+import espeak
 from std_msgs.msg import String
 import os
+import sys
 
-def callback(data):
+def callback(data,args):
 	rospy.loginfo("SpeechServer received speaking command")
-	os.system('espeak "' + data.data + '" --stdout | aplay --device "default:CARD=Device" &')
+	rospy.loginfo("Pyro says: " + data.data)
+	args.say(data.data)
 
 
-def main():
+def main(topic = "/pyro/text_to_speech/input"):
+	# init espeak 
+	espeak.init();
+	speaker = espeak.Espeak()
+	# init ros node
 	rospy.init_node('speech_server',anonymous=True)
-	rospy.Subscriber("/pico/speak",String, callback)
+	rospy.Subscriber(topic, String, callback, speaker)
+	speaker.say("One two, One two, Beep Boop")
 	rospy.spin()
 
 if __name__ == '__main__':
 	try:
-		main()
+		if len(sys.argv) < 2:
+			main()
+		else:
+			main(sys.argv[1])	
+			
 	except rospy.ROSInterruptException:
 		pass
 
