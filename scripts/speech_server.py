963851c96c88c350c41d8f04c99fc066522cb1ed
K de Vos <k.d.vos@tue.nl>
2022-06-07 17:07:45 +0200

    first infra

--- scripts/speech_server.py
+++ scripts/speech_server.py
@@ -3,13 +3,18 @@
 import rospy
 import espeak
 from std_msgs.msg import String
-import os
+import subprocess
 import sys
 
-def callback(data,args):
+def talkcallback(data,args):
 	rospy.loginfo("SpeechServer received speaking command")
 	rospy.loginfo("Pyro says: " + data.data)
 	args.say(data.data)
+	
+def filecallback(data):
+	rospy.loginfo("SpeechServer received play command")
+	rospy.loginfo("Pyro plays: " + data.data)
+	subprocess.run(["ffplay -i", data.data, "-loglevel error -nodisp -autoexit"], shell=True) 
 
 
 def main(topic = "/pyro/text_to_speech/input"):
@@ -18,7 +23,8 @@ def main(topic = "/pyro/text_to_speech/input"):
 	speaker = espeak.Espeak()
 	# init ros node
 	rospy.init_node('speech_server',anonymous=True)
-	rospy.Subscriber(topic, String, callback, speaker)
+	rospy.Subscriber(topic, String, talkcallback, speaker)
+	rospy.Subscriber("/pyro/text_to_speech/file", String, filecallback)
 	speaker.say("One two, One two, Beep Boop")
 	rospy.spin()
 
